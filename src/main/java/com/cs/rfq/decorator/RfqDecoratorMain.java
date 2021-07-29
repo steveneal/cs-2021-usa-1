8215f73a97643d8f471941a21e4d1ca3c5de7f60
gucoo <choug3055@connect.durhamtech.edu>
2021-07-29 11:31:24 -0400

    Add Stream

--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -3,7 +3,10 @@ package com.cs.rfq.decorator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import java.util.Arrays;
 
 public class RfqDecoratorMain {
 
@@ -12,12 +15,31 @@ public class RfqDecoratorMain {
         System.setProperty("spark.master", "local[4]");
 
         //TODO: create a Spark configuration and set a sensible app name
-
+        SparkConf conf = new SparkConf().setAppName("StreamFromRfq");
         //TODO: create a Spark streaming context
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         //TODO: create a Spark session
 
         //TODO: create a new RfqProcessor and set it listening for incoming RFQs
+
+
+        JavaDStream<String> lines = jssc.socketTextStream("localhost", 9000);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+
+        //print out the results
+        words.foreachRDD(rdd -> {
+            rdd.collect().forEach(line -> consume(line));
+        });
+
+
+        jssc.start();
+        jssc.awaitTermination();
+
+}
+
+    static void consume(String line) {
+        System.out.println(line);
     }
 
 }
